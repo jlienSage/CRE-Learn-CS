cc6802bc3ff2b10bc205609decbfa61d66e63c79
John Lien <john.lien@sage.com>
2019-07-17 17:49:02 -0700

    First lesson.

---
+++ Lessons/Lesson1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Lesson1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 0;
+            int y = 0;
+            int z = 0;
+
+            // TODO: write a set of statements that computes the largest of the above values
+            // and prints it to the console in a friendly message.
+
+            Console.WriteLine("Incomplete :(");
+        }
+    }
+}
