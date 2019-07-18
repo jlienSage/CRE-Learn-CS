f86d800d511c9b790f9f68e4059a9cea33761eca
John Lien <john.lien@sage.com>
2019-07-17 18:01:49 -0700

    Add values.

--- Lessons/Lesson1/Program.cs
+++ Lessons/Lesson1/Program.cs
@@ -6,9 +6,9 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            int x = 0;
-            int y = 0;
-            int z = 0;
+            int x = -1;
+            int y = 608;
+            int z = 12;
 
             // TODO: write a set of statements that computes the largest of the above values
             // and prints it to the console in a friendly message.
