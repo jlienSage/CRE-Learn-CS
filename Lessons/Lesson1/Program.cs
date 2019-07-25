10f996854379ebb1d601a8beea22bbc51ebf9fd7
Davis <Richard.Davis@sage.com>
2019-07-25 10:38:09 -0700

    New Branch

--- Lessons/Lesson1/Program.cs
+++ Lessons/Lesson1/Program.cs
@@ -9,11 +9,20 @@ namespace Lesson1
             int x = -1;
             int y = 608;
             int z = 12;
+            int largestNum  = x;
 
             // TODO: write a set of statements that computes the largest of the above values
             // and prints it to the console in a friendly message.
+            if (y > largestNum) 
+            {
+                largestNum = y;
+            }
+            if (z > largestNum)
+            {
+                largestNum = z;
+            }
 
-            Console.WriteLine("Incomplete :(");
+            Console.WriteLine("Largest Number is: " + largestNum);
         }
     }
 }
