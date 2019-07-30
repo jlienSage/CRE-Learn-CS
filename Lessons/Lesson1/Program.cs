987f17e3a54d6d8da591501a4a55294505dd4289
Weyer <Greg.Weyer@sage.com>
2019-07-30 12:08:41 -0700

    My Homework

--- Lessons/Lesson1/Program.cs
+++ Lessons/Lesson1/Program.cs
@@ -6,14 +6,39 @@ namespace Lesson1
     {
         static void Main(string[] args)
         {
-            int x = -1;
-            int y = 608;
+            int x = 100;
+            int y = 6;
             int z = 12;
+
+
+
 
             // TODO: write a set of statements that computes the largest of the above values
             // and prints it to the console in a friendly message.
 
-            Console.WriteLine("Incomplete :(");
+
+            if (x > y)
+            {
+                if (x > z)
+                {
+                    Console.WriteLine("X is the largest number");
+                }
+                else
+                {
+                    Console.WriteLine("Z is the largest number");
+                }
+            }
+            else if (y > z)
+            {
+                Console.WriteLine("Y is the largest number");
+            }
+            else
+            {
+                Console.WriteLine("Z is the largest number");
+            }
+
+
+
         }
     }
 }
