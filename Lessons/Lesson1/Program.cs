d95e782cc9443b0bf6c1f0960d4e3c892e3f2e51
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-08-01 10:21:59 -0700

    Update Program.cs

--- Lessons/Lesson1/Program.cs
+++ Lessons/Lesson1/Program.cs
@@ -14,7 +14,21 @@ namespace Lesson1
             // TODO: write a set of statements that computes the largest of the above values
             // and prints it to the console in a friendly message.
 
+                if (x > y || x > z)
+                {
+                    if (y > x || y > z)
+                    {
 
-                        Console.WriteLine("incomplte:() ");
+                        Console.WriteLine("Sum of two largest values is: "+x+y);
 
-
+                    }
+                
+                }
+                else
+                {
+                    Console.WriteLine("Sum of two largest values is: "+z+y);
+                }
+        }
+            
+    }
+}
