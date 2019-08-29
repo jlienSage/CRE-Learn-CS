30b7c9a9f8ac7c7d4630e74471ecf9fe4c619938
mikaylarmcgraw <50457687+mikaylarmcgraw@users.noreply.github.com>
2019-08-29 09:28:25 -0700

    Update Program.cs

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -1,9 +1,3 @@
-<<<<<<< HEAD
-﻿// TODO: determine whether or not "palindrome" is, in fact, a palindrome.
-=======
-// TODO: determine whether or not "palindrome" is, in fact, a palindrome.
->>>>>>> fbed8c68348e792f889a67c6ef1af516cd1c1843
-// Print a friendly message.
 using System;
 
 namespace Lesson2
@@ -12,31 +6,36 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            //creating input portion for string a
-           string a = string.Empty;
-           Console.WriteLine("Please enter a string:");
-           a = Console.ReadLine();
+            //creating an array of char with parameter past into args
+            char[]checkPalindrome = args[0].ToCharArray();
 
-            //creating my array of char
-            //reversing string
-           char[] palindrome = a.ToCharArray();
-           Array.Reverse(palindrome);
-           string b = new string(palindrome);
+            //initalizing variables
+            int backOfArray = checkPalindrome.Length-1;
+            int i =0;
+            Boolean flag = true;
 
+            while(flag)
+            {
+              if(checkPalindrome[i]==checkPalindrome[backOfArray])  
+              {
+                 if (i==checkPalindrome.Length-1 && backOfArray==0)
+                 {
+                    Console.WriteLine("Yes the text entered is a palindrome");
+                    flag = false;
+                 }
+                 
+                  i++;
+                  backOfArray--;
+                   
+              } 
+              else
+              {
+                  Console.WriteLine("No the text entered is not a palindrome");
+                  flag = false;
+              }
+              
+            }
 
-            //comparing the strings.. outputting friendly message
-           if (a==b)
-           {
-                Console.WriteLine("Yes, the text entered is a palindrome");        
-
-           }
-           else
-           {
-                Console.WriteLine("No, the text entered is not a palindrome.");
-
-           }
-            
         }
-
     }
-}
+ }
