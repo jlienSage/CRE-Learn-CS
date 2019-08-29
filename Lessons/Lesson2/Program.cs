ec242febf801545f30ce4ba343fa3c2ce485da00
Weyer <Greg.Weyer@sage.com>
2019-08-29 10:25:58 -0700

    Lesson 2

--- Lessons/Lesson2/Program.cs
+++ Lessons/Lesson2/Program.cs
@@ -6,12 +6,61 @@ namespace Lesson2
     {
         static void Main(string[] args)
         {
-            var palindrome = args[0].ToCharArray();
+            char[] palindrome = args[0].ToCharArray(); 
+            string palindrome_str = args[0];
+                   
 
             // TODO: determine whether or not "palindrome" is, in fact, a palindrome.
             // Print a friendly message.
 
-            Console.WriteLine("Incomplete :(");
-        }
-    }
+            Console.WriteLine ("Your input: " + "\"" + palindrome_str + "\"");
+             
+            int frontIndex = 0;
+            int backIndex = palindrome.Length -1;
+            bool successfulMatch = true;
+
+
+            for (frontIndex = 0, backIndex = palindrome.Length -1;
+                frontIndex < backIndex;
+                frontIndex++, backIndex--)
+            {    
+
+                if (palindrome[frontIndex] == palindrome[backIndex])
+                
+                {
+
+                }
+
+                
+                
+                else
+                {
+
+                    successfulMatch = false;
+                    break;
+
+                }
+            }
+
+            if (successfulMatch)
+            {
+                Console.WriteLine ("Correct," + "\"" + palindrome_str + "\"" + " is a palindrome.");
+
+            }
+
+            else
+
+            {
+                Console.WriteLine ("Incorrect, " + "\"" + palindrome_str + "\"" + " is not a palindrome.");
+
+            }
+        
+
+        } 
+                
+               
+
+        
+     }
 }
+
