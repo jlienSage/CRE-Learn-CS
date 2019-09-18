cfb98b854bfa8a0b190ef1adb52ee474b8fceb2f
John Lien <john.lien@sage.com>
2019-09-18 15:49:02 -0700

    Lesson 4

--- Lessons/Lesson3/Program.cs
+++ Lessons/Lesson3/Program.cs
@@ -6,14 +6,20 @@ namespace Lesson3
     {
         static void Main(string[] args)
         {
-            /*            
-                Create a class that represents a broken calculator, how it is broken is up to you, get creative!
-                - The class should be able to be configured upon initilization with a value of your choice
-                    that adversly affects its funcionality in some way.
-                    (e.g. all multiplications are off by some factor, or it prints something to the Console when certain values are entered, etc.)
-                    This configuration should be accessible outside the class BUT NOT MODIFIABLE.
-                - Your calculator should have methods for multiplication, addition, subtraction and division.
-                - Your calculator should have a property representing the current value, which can be cleared.
+            /* 
+                Part 1:           
+                    Create a class that represents a broken calculator, how it is broken is up to you, get creative!
+                    - The class should be able to be configured upon initilization with a value of your choice
+                        that adversly affects its funcionality in some way.
+                        (e.g. all multiplications are off by some factor, or it prints something to the Console when certain values are entered, etc.)
+                        This configuration should be accessible outside the class BUT NOT MODIFIABLE.
+                    - Your calculator should have methods for multiplication, addition, subtraction and division.
+                    - Your calculator should have a property representing the current value, which can be cleared.
+                
+                Part 2:
+                    Create a calculator base class without the broken functionality and extend the class with a new class that
+                    overrides the functionality with your broken functionality. This should mostly be refactoring, no new behavior.
+                    You are NOT allowed to modify your demonstration in this method, it should still compile and run.
 
                 Demonstrate your class's functionality by utilizing it within this Main() method.
             */
