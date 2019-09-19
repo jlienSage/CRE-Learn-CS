c6bdd9d2cb18e078d40e62b2e57471c2fe5a5965
Wallace <lisa.wallace@sage.com>
2019-09-19 10:17:03 -0700

    Calculator Fixes

--- Lessons/Lesson3/Calculator.cs
+++ Lessons/Lesson3/Calculator.cs
@@ -4,9 +4,13 @@ namespace Lesson3
 {
     public class Calculator
     {
+        // private field backing field
+        private double result = 0.0;
+        public bool notdone = true;
+
         public double Add(double first, double second)
         {
-            double result = first + second;
+            result = first + second;
             Console.WriteLine("{0} + {1} = {2}", first, second, result);
             if (first == second)
                 Console.WriteLine("Why didn't you multiply by 2 silly!");
@@ -15,31 +19,46 @@ namespace Lesson3
 
         public double Subtract(double first, double second)
         {
-            double result = first - second;
+            result = first - second;
             Console.WriteLine("{0} - {1} = {2}", first, second, result);
             return result;
         }
 
         public double Mutiply(double first, double second)
         {
-            double result = first * second;
+            result = first * second;
             Console.WriteLine("{0} * {1} = {2}", first, second, result);
             if (first == second)
                 Console.WriteLine("You have twins!");
             if (first != second)
-            Console.WriteLine(result = result * 2);
+                Console.WriteLine(result = result * 2);
             //Console.WriteLine("You now have double trouble");
             return result;
         }
 
         public double Divide(double first, double second)
         {
-            double result = first / second;
+            result = first / second;
             Console.WriteLine("{0} / {1} = {2}", first, second, result);
             if (result < first)
                 Console.WriteLine("You are greedy!");
             return result;
-        } 
+        }
+
+        public void Clear()
+        {
+            result = 0.0;
+
+            Console.WriteLine("Result has been cleared");
+            return;
+        }
+        public void Exit()
+        {
+            notdone = false;
+
+            Console.WriteLine("Program has exited");
+            return;
+        }
 
     }
 }
--- Lessons/Lesson3/Program.cs
+++ Lessons/Lesson3/Program.cs
@@ -5,43 +5,52 @@ namespace Lesson3
 {
     public class Program
     {
-
         public static void Main(string[] args)
         {
             double first, second, result;
             char op;
-
-            Console.Write("Enter first value: ");
-            first = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter second value: ");
-            second = Convert.ToDouble(Console.ReadLine());
-            Console.Write("Enter operator (+, -, *, /): ");
-            op = (char)Console.Read();
-            
-            
             Calculator calculator = new Calculator();
-            
-            switch (op)
+
+            while (calculator.notdone)
             {
-                case '+':
-                    result = calculator.Add( first, second);
-                    break;
+                Console.Write("Enter first value: ");
+                first = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter second value: ");
+                second = Convert.ToDouble(Console.ReadLine());
+                Console.Write("Enter operator (+, -, *, /, C, X,): ");
+                op = (Char)Console.Read();
+                string fake = Console.ReadLine(); // gobble carriage return
 
-                case '-':
-                    result = calculator.Subtract( first, second);
-                   break;
 
-                case '*':
-                    result = calculator.Mutiply( first, second);
-                    break;
+                switch (op)
+                {
+                    case '+':
+                        result = calculator.Add(first, second);
+                        break;
 
-                case '/':
-                     result = calculator.Divide( first, second);
+                    case '-':
+                        result = calculator.Subtract(first, second);
+                        break;
 
-                    break;
+                    case '*':
+                        result = calculator.Mutiply(first, second);
+                        break;
 
-                default:
-                    break;
+                    case '/':
+                        result = calculator.Divide(first, second);
+                        break;
+
+                    case 'C':
+                        calculator.Clear();
+                        break;
+
+                    case 'X':
+                        calculator.Exit();
+                        break;
+
+                    default:
+                        break;
+                }
             }
         }
     }
