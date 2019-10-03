8eb5b7d6cec129714209468196c212a9e8a7953d
Lien <John.Lien@sage.com>
2019-10-03 11:19:55 -0700

    Fix instructions.

--- Lessons/Lesson3/Program.cs
+++ Lessons/Lesson3/Program.cs
@@ -12,7 +12,6 @@ namespace Lesson3
                     - The class should be able to be configured upon initilization with a value of your choice
                         that adversly affects its funcionality in some way.
                         (e.g. all multiplications are off by some factor, or it prints something to the Console when certain values are entered, etc.)
-                        This configuration should be accessible outside the class BUT NOT MODIFIABLE.
                     - Your calculator should have methods for multiplication, addition, subtraction and division.
                     - Your calculator should have a property representing the current value, which can be cleared.
                 
