4b3747c0c4f2dbfdba7779c2f25dfef231839cb4
Davis <Richard.Davis@sage.com>
2019-10-03 10:51:02 -0700

    Lesson 2

---
+++ Lessons/new_folder1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Exercise;
+
+namespace new_folder1
+{
+    class Program
+   {
+        static void Main(string[] args)
+        {
+            // Console.WriteLine("Hello World!");
+            //Person person1 = new Person("", "");
+            Person person1 = new Person();
+
+            person1.HairColor = "brunette";
+
+            Console.WriteLine ("Name: " + person1.Name);
+            Console.WriteLine ("Hair Color: " + person1.HairColor);
+
+        }
+    }
+}
