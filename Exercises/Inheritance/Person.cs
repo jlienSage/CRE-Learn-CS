9d8eb674a98be0e5546724b7d5bedc0fae536fbc
John Lien <john.lien@sage.com>
2019-10-10 09:23:27 -0700

    Exercise

---
+++ Exercises/Inheritance/Person.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Inheritance
+{
+    public abstract class Person
+    {
+        protected abstract decimal Tenure {get;}
+        protected abstract string Name {get;}
+
+        public override string ToString()
+        {
+            return $"My name is {Name}. I have been at Sage {Tenure} years.";
+        }
+    }
+}
---
+++ Exercises/Inheritance/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Inheritance
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var people = new List<Person>();
+            // TODO add people
+            
+            foreach(var person in people)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
