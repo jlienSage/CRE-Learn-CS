a1a42e61ab9c0755c9907a0e9c69fed1cefb9045
Davis <Richard.Davis@sage.com>
2019-10-14 11:17:59 -0700

    Rich class

---
+++ Exercises/Inheritance/Rich.cs
@@ -0,0 +1,20 @@
+using System;
+
+namespace Inheritance
+{
+    public  class Rich : Person
+    {
+        protected override decimal Tenure {get;} = 19;
+        protected override string Name => "Rich";
+        //public string Location = "Beaverton";
+        public string Location {get;} = "Beaverton";
+
+        public  override string ToString()
+        {
+            //return $"My name is {Name}. I have been at Sage {Tenure} years.";
+            //return (base.ToString() + "(I like C#)");
+            return (base.ToString() + $"I work in {Location}");
+            //Console.WriteLine ("Hello" + base.ToString() + $"I work in {Location}");
+        }
+    }
+}
