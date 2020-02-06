4ba4d3df648f68411863409f4d34e533824e91cd
Wallace <lisa.wallace@sage.com>
2020-02-06 07:52:38 -0800

    Checkin g in changes for HashSet

--- Lessons/Lesson10/HashSet.cs
+++ Lessons/Lesson10/HashSet.cs
@@ -12,8 +12,27 @@ namespace DataStructure
             Console.WriteLine("HashSet Example: ");
             Console.WriteLine("----------------------------- ");
             // Creating a HashSet of even numbers from 10 to 40 and display using foreach or for loop
-            HashSet<int> odd = new HashSet<int>();
+            HashSet<int> evenNumbers = new HashSet<int>();
 
+            for (int i = 5; i < 21; i++)
+            {
+                // Populate numbers with just even numbers.
+                evenNumbers.Add(i * 2);
+            }
+
+            Console.Write("evenNumbers contains {0} elements: ", evenNumbers.Count);
+            DisplaySet(evenNumbers);
+
+            //Display items in HashSet
+            void DisplaySet(HashSet<int> set)
+            {
+                Console.Write("{");
+                foreach (int i in set)
+                {
+                    Console.Write(" {0}", i);
+                }
+                Console.WriteLine(" }");
+            }
         }
     }
 }
--- Lessons/Lesson10/Program.cs
+++ Lessons/Lesson10/Program.cs
@@ -14,7 +14,7 @@ namespace DataStructure
             //Lesson10.LinkedList();
             //Lesson10.Hashtable();
             Lesson10.Dictionary();
-            //Lesson10.HashSet();
+            Lesson10.HashSet();
             //Lesson10.Stack();
             //Lesson10.Queue();
         }
