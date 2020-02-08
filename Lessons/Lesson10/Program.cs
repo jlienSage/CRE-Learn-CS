b309882a16d141c0f87d6e1233252624c6cc4c97
Wallace <lisa.wallace@sage.com>
2020-02-07 16:58:19 -0800

    Checking in Stack Code.

--- Lessons/Lesson10/Program.cs
+++ Lessons/Lesson10/Program.cs
@@ -15,7 +15,7 @@ namespace DataStructure
             //Lesson10.Hashtable();
             Lesson10.Dictionary();
             Lesson10.HashSet();
-            //Lesson10.Stack();
+            Lesson10.Stack();
             //Lesson10.Queue();
         }
     }
--- Lessons/Lesson10/Stack.cs
+++ Lessons/Lesson10/Stack.cs
@@ -1,5 +1,8 @@
 
 using System;
+using System.Collections;
+
+
 namespace DataStructure
 {
     public partial class Lesson10
@@ -11,21 +14,89 @@ namespace DataStructure
             Console.WriteLine("----------------------------- ");
             //Homework: Create a Stack and fill in the lines as commented. See below for more details.
 
-            // Create a Stack
+            // C# program to illustrate how to 
+            // Create a stack 
+            // Using Stack class
+
+            Stack CoWorker_stack = new Stack();
 
 
+            // Adding elements in the Stack 
+            // Using Push method 
+            // Add all the students name who are in the class in the Stack one by one
+
+            CoWorker_stack.Push("Lisa");
+            CoWorker_stack.Push("Greg");
+            CoWorker_stack.Push("Rich");
+
+            Console.WriteLine("Total elements present in" +
+                        " my_stack: {0}", CoWorker_stack.Count);
+
+            Console.Write("Display of current coworkers after added:");
+            PrintValuesCoWorker(CoWorker_stack);
 
             // Add 'Aman' and 'John' in the Stack
 
-            // Add all the students name who are in the class in the Stack one by one
+            Console.WriteLine("Adding Aman and John to the Stack");
 
 
 
+            CoWorker_stack.Push("Aman");
+            CoWorker_stack.Push("John");
+
+            Console.WriteLine("Total elements present in" +
+                        " my_stack: {0}", CoWorker_stack.Count);
+
+            Console.Write("Displaying Values in Stack after adding John and Aman:");
+            PrintValuesCoWorker(CoWorker_stack);
 
             // remove only 'Aman' from the stack and maintain the order of the stack. Write your logic and display stack
 
+            var Name = CoWorker_stack.Pop();
+            {
+                if (Name == "Aman")
+                {
 
+                    Console.Write("\tValues:");
+                    PrintValuesCoWorker(CoWorker_stack);
+                }
+                else
+                {
+                    //Remove Aman from the list but keep everything in order.
+                    Console.WriteLine("Popping '{0}'", CoWorker_stack.Pop());
+                    Console.Write("\tValues:");
+                    PrintValuesCoWorker(CoWorker_stack);
+                    Console.Write("Adding John back into stack");
+                    CoWorker_stack.Push("John");
+                    PrintValuesCoWorker(CoWorker_stack);
+
+                }
+            }
+        }
+
+        public static void PrintValuesCoWorker(IEnumerable myCollection)
+        {
+            foreach (Object obj in myCollection)
+                Console.Write("    {0}", obj);
+            Console.WriteLine();
 
         }
+
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
